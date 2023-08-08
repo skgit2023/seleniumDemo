70687dc8ab60d37b9af48728197afdcdbad6e817
skgit2023 <sk1994501@gmail.com>
2023-08-08 07:27:07 +0530

    this is my first commit

---
+++ SeleniumDemo/src/ChromeDemo.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class ChromeDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		System.setProperty("webdriver.chrome.driver","E:\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver= new ChromeDriver();
+	
+	
+	
+	
+	//driver.get("https://www.gmail.com");
+	//driver.manage().window().maximize();
+	//ChromeDriver driver = new ChromeDriver();
+		//RemoteWebDriver driver = new ChromeDriver();
+		
+	
+	
+	}
+	
+}
